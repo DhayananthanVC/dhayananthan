86240f2cb2c80f13d5793e37b3216ee71732661d
DhayananthanVC <35652016+DhayananthanVC@users.noreply.github.com>
2018-01-25 00:02:43 +0530

    Create greatest no.c

---
+++ greatest no.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a[i],b=0;
+printf("enter the no 1,2 and 3")
+for(i=0;i<3;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<3;i++)
+{
+
+if(b>a[i])
+{
+b=a[i+1];
+}
+else
+{
+b=a[i];
+}
+}
+printf("%d is greater",b);
+getch();
+}
