8c3fe4d465d088d5a298497fcfb80948dbac98db
DhayananthanVC <35652016+DhayananthanVC@users.noreply.github.com>
2018-01-25 16:38:58 +0530

    Create sum of n terms.c

---
+++ sum of n terms.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+int i,N[i],c=0,T,K;
+printf(" get sum upto and no. of integers ");
+scanf("%d%d",&K,&T);
+for(i=1;i<=T;i++)
+{
+scanf("%d",&N[i]);
+}
+
+for(i=1;i<=K;i++)
+{
+c=c+N[i];
+}
+printf(" %d is sum",c);
+}
