5d1c1bb69d41fde1e81b170ed8170553f4a5f6e5
DhayananthanVC <35652016+DhayananthanVC@users.noreply.github.com>
2018-02-11 10:57:53 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int i,a[i],n,b;
+    printf("enter no. of elements needed");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+        
+    }
+    b=a[n];
+    for(i=1;i<=n;i++)
+    {
+        if(b<a[i])
+        {
+            b=a[i];
+        }
+    }
+    printf("\n %d is maximum",b);
+    
+}
