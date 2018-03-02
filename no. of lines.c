726a007175a53413d8c0785c36cb2b12ac694423
DhayananthanVC <35652016+DhayananthanVC@users.noreply.github.com>
2018-03-02 14:11:21 +0530

    Create no. of lines.c

---
+++ no. of lines.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[50];
+int b=1,i;
+printf("enter the string to count");
+scanf("%[^\n]s",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]=='.')
+{
+b++;
+}
+}
+printf("\n %d sentences",b);
+}
