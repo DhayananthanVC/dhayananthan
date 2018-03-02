fe2b1db37d4edf01ce7c49a9de9aeb6ccda576c3
DhayananthanVC <35652016+DhayananthanVC@users.noreply.github.com>
2018-03-02 15:18:38 +0530

    Create prstr.c

---
+++ prstr.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a[50];
+int n;
+printf("enter the string");
+scanf("%s",a);
+printf("enter the no");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("\n %s",a);
+}
+}
