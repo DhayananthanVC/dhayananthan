27cd306ceebc50777991a6c6a275f1d5ffb7d4f1
DhayananthanVC <35652016+DhayananthanVC@users.noreply.github.com>
2018-03-08 16:53:39 +0530

    Create conwstrcat.c

---
+++ conwstrcat.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+char a[100],b[50];
+int i,n,c=0;
+printf("enter the words ");
+scanf("%s%s",a,b);
+for(i=0;i<=100;i++)
+{
+if(a[i]='\0')
+{
+  
+ i=n;
+}
+}
+
+do{
+    a[n]=b[c];
+    n++;
+    c++;
+}while(b[c]!='\0');
+for(i=0;a[i]!='\0';i++)
+{
+    printf("%s",a[i]);
+}
+    }
