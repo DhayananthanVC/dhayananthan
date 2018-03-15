def3e6b59dca84f60349bc72c54030c1d28b76e3
DhayananthanVC <35652016+DhayananthanVC@users.noreply.github.com>
2018-03-15 16:45:21 +0530

    Create num to wor.c

---
+++ num to wor.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the no.");
+    scanf("%d",&a);
+    if(a==1)
+    {
+        printf("one");
+    }
+    else if(a==2)
+    {
+        printf("two");
+    }
+    else if(a==3)
+    {
+        printf("three");
+    }
+     else if(a==4)
+    {
+        printf("four");
+    }
+     else if(a==5)
+    {
+        printf("five");
+    }
+     else if(a==6)
+    {
+        printf("six");
+    }
+     else if(a==7)
+    {
+        printf("seven");
+    }
+     else if(a==8)
+    {
+        printf("eight");
+    }
+     else if(a==9)
+    {
+        printf("nine");
+    }
+     else if(a==10)
+    {
+        printf("ten");
+    }
+}
