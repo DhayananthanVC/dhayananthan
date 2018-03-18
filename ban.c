f2f43e316a5b3cf669b9b9197c4163423d6d9ed6
DhayananthanVC <35652016+DhayananthanVC@users.noreply.github.com>
2018-03-18 04:36:47 +0000

    Update ban.c

--- ban.c
+++ ban.c
@@ -2,18 +2,19 @@
 void main()
 {
     char a[100];
-    int b=0,n,i;
+    int b=0,n,i,c=0;
     printf("enter the string");
     scanf("%s",a);
     for(i=0;a[i]!='\0';i++)
     {
-        if(a[i]>='a' && a[i]<='z' || a[i]>='1' && a[i]<='9')
+        if(a[i]>='a' && a[i]<='z')
         {
             b++;
         }
+         if(a[i]>='1' && a[i]<='9')
     }    
      n=strlen(a);
-     if(b==n)
+     if(b==1 && c==1)
   {
       printf("yes");
   }
